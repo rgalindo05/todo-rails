fc6a4f2950f73b058dfd1dcf89a11df880bf6545
Steve McKinney <steve.mckinney@gmail.com>
2012-05-21 16:07:09 -0400

    Adds ensure user is signed in

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -8,6 +8,11 @@ When /^I sign in as "(.*?)"$/ do |email_address|
   click_button 'Sign in'
 end
 
+Then /^I should be prompted to sign in$/ do
+  page.should have_css('.flash.notice', text: 'Please sign in')
+  page.should have_css("form[action='#{sessions_path}']")
+end
+
 Then /^I should see that I have signed in as "(.*?)"$/ do |email_address|
   within 'header' do
     page.should have_css("[data-current-user='#{email_address}']")
