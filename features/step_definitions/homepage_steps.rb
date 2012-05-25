08fef20bba0cd96c15fc14a4f50dd1a3c1cde584
Steve McKinney <steve.mckinney@gmail.com>
2012-05-24 20:53:38 -0400

    Adds marketing component and simplified login form

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -3,5 +3,6 @@ When /^I view the homepage$/ do
 end
 
 Then /^I should be able to read about the application$/ do
+  page.should have_css('[data-role=title]')
   page.should have_css('[data-role=description]')
 end
